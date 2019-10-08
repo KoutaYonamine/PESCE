7e2de455f1eb3f95d807db8e07957e65b30b13d8
rongbiiti <11807078@stu.kbc.ac.jp>
2019-10-08 15:34:40 +0900

    点数管理、クールタイム管理（使わないけど）、ヒットボックスと当たり判定を作成。 魚にコライダーとrigidbody、ぐるくん・たまんはタグをFish、トビウオはタグをPesceにすること

---
+++ Assets/hayato/PointPlusMethod.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointPlusMethod : MonoBehaviour
+{
+    public int Fish_0_point;
+    public int Fish_1_point;
+    public int Fish_2_point;
+
+    private int Point;  // 合計点
+
+    void Start()
+    {
+        Point = 0;
+    }
+
+    public int GetPoint()
+    {
+        return Point;
+    }
+
+    // 0...ぐるくん、1...トビウオ、2...タマン。
+    public void PointPlus(int FishType)
+    {
+        if(FishType == 0)
+        {
+            Point += Fish_0_point;
+        } else if (FishType == 1)
+        {
+            Point += Fish_1_point;
+        } else if (FishType == 2)
+        {
+            Point += Fish_2_point;
+        }
+    }
+}
