d624458d499f975974a40a49cbb504cd3977f7f9
TITAN0527 <11806083@stu.kbc.ac.jp>
2019-10-10 14:07:33 +0900

    新リザルト完成です

--- Assets/hayato/Timer.cs
+++ Assets/hayato/Timer.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 public class Timer : MonoBehaviour
 {
     public float countTime = 60;
+    public GameObject Result;
 
     // Use this for initialization
     void Start()
@@ -17,5 +18,10 @@ public class Timer : MonoBehaviour
     {
         countTime -= Time.deltaTime; //スタートしてからの秒数を格納
         GetComponent<Text>().text = countTime.ToString("F0") + "秒"; //小数2桁にして表示
+        if(countTime <= 0)
+        {
+            Instantiate(Result);
+            Destroy(gameObject);
+        }
     }
 }
